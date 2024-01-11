e49828267692a752cd9fa598657362189b92376a
Connor Davis, PhD <connorjdavis1@gmail.com>
2024-01-10 21:48:34 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Purpose
+
+This is a part of a larger project that employs corpus methods to examine the role of variability as a predictor of a composer’s state of health. The specific focus of this study is the effect of syphilis—broadly defined as unwellness—on Franz Schubert’s musical output. Measures of scale-degree, interval, and rhythmic variability, are used utilized a binomial logistic regression. Two musical features were significant (P<.01) at predicting the episodes of unwellness in the composer’s output, highlighting the link between biographical events and artistic output.
+
+Daniel Shanahan is a co-author on this project.
+
+#organization
+
+-The Quarto schubertWellness.qmd has been exported as a pdf and html for convenience
+-the code is all in schubertWellness.r 
+-Data is provided as schubertData.csv
---
+++ schubertWelness.R
@@ -0,0 +1,86 @@
+#---
+setwd("/Users/connordavis/Desktop/projects/schubert/")
+schubert_data <- read.csv("schubert_info3.csv", header=T)
+library(tidyverse)
+library(GGally)
+library(effects) #for plotting parameter effects
+#---
+
+#This data set is musical features from the Schubert melodies throughout
+#his lifetime along with information pertaining to his wellness and unwellness.
+#The aim of this project is to see if there is a difference in his musical output
+#in the different unwell periods and what musical features are present if a
+#difference exists
+
+#ClEANING#
+
+str(schubert_data)
+
+schubertDataClean <- schubert_data %>% 
+  filter(!str_detect(wellness, "uncertain")) %>% #get rid of pieces we aren't certain about his health
+  select(-NOTES..to.be.deleted., #remove unecessary columns
+         -completion_date,
+         -deutsche_num,
+         -filename) %>% 
+  filter(!str_detect(start_date, "UNKNOWN")) %>% #remove unknowns from the dates
+  drop_na() 
+
+schubertDataClean$wellness <- if_else(schubertDataClean$wellness == "well", 1, 0)
+schubertDataClean$start_date <- as.integer(schubertDataClean$start_date)
+
+
+###Checking correlations and normality
+
+schubertDataClean %>% #looks like normal disctributions and degree_entropy and intervallic_entropy are positively correlated.
+  select(degree_entropy, intervallic_entropy, npvi, wellness) %>% 
+  ggpairs() 
+
+
+###GLM Test for Significant variables
+glmTest <- glm(formula = wellness ~ degree_entropy + #npvi and degree_entropy are significant
+                 intervallic_entropy + 
+                 npvi,
+               data = schubertDataClean, 
+               family = binomial(link = "logit"))
+
+summary(glmTest) #npvi and degree_entropy are significant at predicting wellness
+
+plot(allEffects(glmTest)) ##to visualize the effect we can see the positive and negative influences of our two significant variables
+
+###Testing Fit and Evaluating Parameters
+
+glmSchubert <- glm(formula = wellness ~ degree_entropy + #npvi and degree_entropy are significant
+                               intervallic_entropy,
+                             data = schubertDataClean, 
+                             family = binomial(link = "logit"))
+
+anova(glmTest, glmSchubert, test ="Chisq") #evaluating test against model without npvi. Model without npvi is better, so we will proceed with that. Note chi-squared.
+
+glmTest$aic
+glmSchubert$aic #just to further check, and glmSchubert is definitely better.
+
+ggplot(schubertDataClean, aes(x = degree_entropy+npvi, y = wellness)) + 
+  geom_point() +
+  stat_smooth(method = "glm", method.args = list(family=binomial), se = TRUE)
+
+##plotting our two variables for an elbow, which we can see beginning at the unwell period starting in 1822 when he developed syphilis.
+
+ggplot(schubertDataClean, aes(start_date, degree_entropy, wellness)) +
+  geom_rect(aes(NULL, NULL,
+                xmin=start_date - .5 , xmax=start_date +.5,
+                ymin=min(degree_entropy), ymax=max(degree_entropy),
+                fill=wellness
+                )) +
+    geom_point() + 
+    geom_smooth()
+
+ggplot(schubertDataClean, aes(start_date, npvi, wellness)) +
+  geom_rect(aes(NULL, NULL,
+                xmin=start_date - .5 ,xmax=start_date +.5,
+                ymin=min(npvi), ymax=max(npvi),
+                fill=wellness)) +
+  geom_point() + 
+  geom_smooth()
+
+#it seems there is a difference in at least melodic output when a composer is unwell.
+
